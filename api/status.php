552f5ab5f8cdd36df10e30d2251a1e97fa84bd28
LAUTTOM <lau250422@outlook.com>
2025-05-08 21:59:09 +0700

    Create status.php

---
+++ api/status.php
@@ -0,0 +1,21 @@
+<?php
+// 获取支付信息
+$transactionID = $_POST['transaction_id'] ?? 'No transaction ID';
+$orderID = $_POST['order_id'] ?? 'No order ID';
+$amount = $_POST['amount'] ?? '0';
+$status = $_POST['status'] ?? 'Unknown';
+$date = date("Y-m-d H:i:s");
+
+// 邮件内容
+$to = "lau240708@outlook.com";
+$subject = "Payeer Payment Notification";
+$message = "Payment Status: $status\n";
+$message .= "Transaction ID: $transactionID\n";
+$message .= "Order ID: $orderID\n";
+$message .= "Amount: $amount\n";
+$message .= "Date: $date\n";
+
+// 发送邮件
+mail($to, $subject, $message);
+echo "Notification sent.";
+?>
